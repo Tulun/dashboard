92ad420daf25d418b1f7b3be3b27882c6c4db9e2
Tulun <jaykiraly@gmail.com>
2023-02-21 16:51:30 -0800

    types styled components
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
+import { init, useConnectWallet } from "@web3-onboard/react";
+import injectedModule from "@web3-onboard/injected-wallets";
+import { ethers } from "ethers";
+
+const injected = injectedModule();
+
+const infuraKey = process.env.REACT_APP_INFURA_KEY;
+const rpcUrl = `https://mainnet.infura.io/v3/${infuraKey}`;
+
+// initialize Onboard
+init({
+  wallets: [injected],
+  chains: [
+    {
+      id: "0x1",
+      token: "ETH",
+      label: "Ethereum Mainnet",
+      rpcUrl,
+    },
+  ],
+});
+
 function App() {
+  const [{ wallet, connecting }, connect, disconnect] = useConnectWallet();
+
+  if (wallet) {
+    // ethersProvider = new ethers.providers.Web3Provider(wallet.provider, "any");
+    console.log("ethersProvider", ethers);
+  }
+
   return (
     <div>
-      <h1>React TypeScript</h1>
+      <button
+        disabled={connecting}
+        onClick={() => (wallet ? disconnect(wallet) : connect())}
+      >
+        {connecting ? "connecting" : wallet ? "disconnect" : "connect"}
+      </button>
     </div>
   );
 }
